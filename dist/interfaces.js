1857b661ed0e403e952643effaf4af2ffc73e3b2
nishant Gupta <kr.nish99@gmail.com>
2021-10-06 17:27:28 +0530

    initilization of interface and usage

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -1,3 +1,4 @@
+// so now we have used the reserved keyword of ts that is interface in which we define the data types for the variables and make sure the name of the interface starts with a capital letter 
 var user = {
     name: "Nishant",
     age: 35
@@ -5,3 +6,4 @@ var user = {
 var user2 = {
     name: "Pratirath"
 }; // but for soemreason if i for get to add age for a second object named user 2 ts is nto going to warn me so although i can write data type in every object like {name:string , age: number} so now when we see ts is twlling us that age is missing from user 2  but copying it and pasting it with every object is a lot of work hence we use intefaces  
+// now instead of the entire thing we can just say user 2 belongs to a User interface and it should expect name and age with there respective parameters 
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,17 @@
+interface User {
+    name: string;
+    age: number;
+}
+// so now we have used the reserved keyword of ts that is interface in which we define the data types for the variables and make sure the name of the interface starts with a capital letter 
+
+
 const user :{name:string , age: number} = {
     name: "Nishant",
     age: 35
 }; // if we hover over user we can see that ts understands that the data we entered for name is string and for age its a int
 
 
-const user2:{name:string , age: number} = {
+const user2: User = {
     name: "Pratirath",
 } // but for soemreason if i for get to add age for a second object named user 2 ts is nto going to warn me so although i can write data type in every object like {name:string , age: number} so now when we see ts is twlling us that age is missing from user 2  but copying it and pasting it with every object is a lot of work hence we use intefaces  
+// now instead of the entire thing we can just say user 2 belongs to a User interface and it should expect name and age with there respective parameters 
