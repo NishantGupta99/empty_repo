ddc6dd36ef2eb03d9808bc51dc729ba21885c61e
nishant Gupta <kr.nish99@gmail.com>
2021-10-06 14:57:58 +0530

    configuring ts

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+var a = "1";
+console.log("aaa", a); // if there is an error that we make in ts it can be seen without runnin the code by howvering over the code and also by running the code in watch mode by adding tsc followed by the file name and -w 
--- main.ts
+++
@@ -1,2 +0,0 @@
-const a = "1"; 
-console.log("aaa" , a) // if there is an error that we make in ts it will automatically 
