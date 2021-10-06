ea2e106a5d091e304e5db90347d79448736ba13e
nishant Gupta <kr.nish99@gmail.com>
2021-10-06 16:08:50 +0530

    first setp to learn about data type in type script

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,2 @@
-var a = "1";
-console.log("aaa", a); // if there is an error that we make in ts it can be seen without runnin the code by howvering over the code and also by running the code in watch mode by adding tsc followed by the file name and -w 
+var hello = "world"; // if we hover our mouse over the hello word we can see ts is returning a data type as world weher as in js we will get it as string 
+hello = "nishant"; // and if we try to reasign the value it will give us an error here on the coding page aswell as the watch mode because we have defined hello as a constant 
