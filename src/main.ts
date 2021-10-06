1e627c183fb1da29a2c690d5bd150f1256b57bf5
nishant Gupta <kr.nish99@gmail.com>
2021-10-06 16:12:29 +0530

    how to use data types

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
-const hello = "world" ; // if we hover our mouse over the hello word we can see ts is returning a data type as world weher as in js we will get it as string 
-hello = "nishant" ; // and if we try to reasign the value it will give us an error here on the coding page aswell as the watch mode because we have defined hello as a constant 
+let hello = "world" ; // if we hover our mouse over the hello word we can see ts is returning a data type as world weher as in js we will get it as string 
+hello = "Bangalore" ; // and if we try to reasign the value it will give us an error here on the coding page aswell as the watch mode because we have defined hello as a constant that means we cannot redefine hello but if we replace const with let we can redefine the same. 
+let firstName :string = " nishant"; // in order to set name as a stirng we will use : after the variable or constant name 
+firstName = 7; // now if we try to reassign the value of firstName as a number or boolean value it will give us error here itself but we can always save it to a string 
+
 
